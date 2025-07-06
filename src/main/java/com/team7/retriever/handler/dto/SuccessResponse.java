55260ebbf17ce11507795433e8fd0a9e220cc88b
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-07 00:07:28 +0900

    Feat: 성공 응답 DTO 추가
    - 추후 반영 예정

---
+++ src/main/java/com/team7/retriever/handler/dto/SuccessResponse.java
@@ -0,0 +1,17 @@
+package com.team7.retriever.handler.dto;
+
+import com.team7.retriever.exception.base.BaseSuccessCode;
+
+public record SuccessResponse<T>(
+	int status,
+	String message,
+	T data
+) {
+	public static <T> SuccessResponse<T> of(final BaseSuccessCode baseSuccessCode, final T data) {
+		return new SuccessResponse<>(baseSuccessCode.getStatus(), baseSuccessCode.getMessage(), data);
+	}
+
+	public static <T> SuccessResponse<T> from(final BaseSuccessCode baseSuccessCode) {
+		return new SuccessResponse<>(baseSuccessCode.getStatus(), baseSuccessCode.getMessage(), null);
+	}
+}

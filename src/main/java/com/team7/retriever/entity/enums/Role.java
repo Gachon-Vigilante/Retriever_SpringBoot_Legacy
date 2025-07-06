9661841344d2295f85e20f6d85793836209aee1f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-07 00:08:23 +0900

    Refactor: 사용자 `Role` 수정

--- src/main/java/com/team7/retriever/entity/enums/Role.java
+++ src/main/java/com/team7/retriever/entity/enums/Role.java
@@ -1,7 +1,23 @@
 package com.team7.retriever.entity.enums;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum Role {
-	ROLE_ROOT,
-	ROLE_ADMIN,
-	ROLE_USER
+
+	ROOT("ROLE_ROOT"),
+	ADMIN("ROLE_ADMIN"),
+	USER("ROLE_USER"),
+	GUEST("ROLE_GUEST");
+
+	private final String roleName;
+
+	public GrantedAuthority toGrantedAuthority() {
+		return new SimpleGrantedAuthority(roleName);
+	}
 }

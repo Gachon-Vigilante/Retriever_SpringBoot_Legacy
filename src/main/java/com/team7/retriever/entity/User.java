deec2bf9c940de3a5efc19e550b4d38e3572e558
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-11 03:47:12 +0900

    Delete: `active` 필드 삭제

--- src/main/java/com/team7/retriever/entity/User.java
+++ src/main/java/com/team7/retriever/entity/User.java
@@ -31,8 +31,6 @@ public class User {
 
 	private Role role;
 
-	private boolean active;
-
 	@CreatedDate
 	private LocalDateTime createdAt;
 
@@ -45,7 +43,6 @@ public class User {
 			.password(password)
 			.name(name)
 			.role(role)
-			.active(false)
 			.build();
 	}
 

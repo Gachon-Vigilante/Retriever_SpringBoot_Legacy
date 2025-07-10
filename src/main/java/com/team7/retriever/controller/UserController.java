c8fd834c84486182862bc8ae24152e611e12e776
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-11 04:02:40 +0900

    Feat: 권한 부여 기능 API 추가

--- src/main/java/com/team7/retriever/controller/UserController.java
+++ src/main/java/com/team7/retriever/controller/UserController.java
@@ -2,10 +2,14 @@ package com.team7.retriever.controller;
 
 import java.util.List;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.team7.retriever.dto.request.GrantRequest;
 import com.team7.retriever.dto.response.UserInfoResponse;
 import com.team7.retriever.service.UserService;
 
@@ -21,5 +25,10 @@ public class UserController {
 	@GetMapping
 	public List<UserInfoResponse> getUsers() {
 		return userService.getAllUsers();
+	}
+
+	@PatchMapping("/grant-role")
+	public String grantRole(@AuthenticationPrincipal String adminId, @RequestBody GrantRequest grantRequest) {
+		return userService.grantRole(adminId, grantRequest);
 	}
 }

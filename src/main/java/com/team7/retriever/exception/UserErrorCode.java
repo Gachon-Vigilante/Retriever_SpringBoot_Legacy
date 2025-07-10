ccf884dc6c685ef3ceffd585a7ebf68e146e975a
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-11 03:57:02 +0900

    Feat: `Root` 권한 부여에 대한 예외 처리 추가

--- src/main/java/com/team7/retriever/exception/UserErrorCode.java
+++ src/main/java/com/team7/retriever/exception/UserErrorCode.java
@@ -13,7 +13,8 @@ public enum UserErrorCode implements BaseErrorCode {
 	ALREADY_GRANTED_ROLE(400, "이미 해당 역할이 부여된 사용자입니다"),
 	ALREADY_EXISTS(400, "이미 존재하는 사용자입니다"),
 	CAN_NOT_CHANGE_MY_ROLE(400, "본인의 역할은 변경할 수 없습니다"),
-	CAN_NOY_CHANGE_ADMIN_ROLE(400, "관리자 역할은 변경할 수 없습니다"),
+	CAN_NOT_CHANGE_ADMIN_ROLE(400, "관리자 역할은 변경할 수 없습니다"),
+	CAN_NOT_GRANT_ROOT_ROLE(400, "Root 역할은 부여할 수 없습니다"),
 	PASSWORD_MISMATCH(400, "비밀번호가 일치하지 않습니다"),
 	UNAUTHORIZED(401, "승인되지 않은 사용자 입니다"),
 	NOT_FOUND(404, "사용자를 찾을 수 없습니다");

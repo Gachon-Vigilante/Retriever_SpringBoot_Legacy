13b279c4ac87f80e9a3c5800d98830926577bea7
Jimin Jung <wojoei1933@gmail.com>
2025-07-16 23:33:04 +0900

    Style: 주석 처리했던 사용할 수 없는 로직 삭제

--- src/main/java/com/team7/retriever/neo4j/controller/PostController.java
+++ src/main/java/com/team7/retriever/neo4j/controller/PostController.java
@@ -1,13 +1,12 @@
 package com.team7.retriever.neo4j.controller;
 
-import com.team7.retriever.neo4j.entity.Posts;
+import com.team7.retriever.neo4j.entity.Post;
 import com.team7.retriever.neo4j.service.PostService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,32 +17,8 @@ import java.util.List;
 public class PostController {
 	private final PostService postService;
 
-    /*
-    // 데이터 많으면 실행 X (드라이버 연결 끊어짐)
-    @GetMapping
-    public List<Posts> getAllPosts() {
-        return postService.getAllPosts();
-    }
-
-    @GetMapping("/depth")
-    public List<Posts> getPostsDepth1() {
-        return postService.getAllPostsDepth1();
-    }
-     */
-
-    /*
-    // paging
-    @GetMapping("/paged")
-    public List<Posts> getPagedPosts(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "100") int size
-    ) {
-        return postService.getPagedPosts(page, size);
-    }
-     */
-
 	@GetMapping("/streamed")
-	public List<Posts> getAllPostsStreamed() {
+	public List<Post> getAllPostsStreamed() {
 		return postService.getAllPostsStreamed();
 	}
 
--- src/main/java/com/team7/retriever/neo4j/repository/NeoPostsRepository.java
+++ src/main/java/com/team7/retriever/neo4j/repository/NeoPostsRepository.java
@@ -1,6 +1,6 @@
 package com.team7.retriever.neo4j.repository;
 
-import com.team7.retriever.neo4j.entity.Posts;
+import com.team7.retriever.neo4j.entity.Post;
 
 import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
@@ -8,27 +8,13 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 @Repository
-public interface NeoPostsRepository extends Neo4jRepository<Posts, String> {
+public interface NeoPostsRepository extends Neo4jRepository<Post, String> {
 
-	Optional<Posts> findByPostId(String postId);
-
-	/*
-	// paging
-	@Query("""
-		    MATCH (p:Post)
-		    OPTIONAL MATCH (p)-[pr:PROMOTES]->(c:Channel)
-		    OPTIONAL MATCH (p)-[sim:SIMILAR]->(sp:Post)
-		    RETURN p, collect(pr), collect(c), collect(sim), collect(sp)
-		    SKIP $skip LIMIT $limit
-		""")
-	List<Posts> findAllWithPromotesAndSimilarPaged(@Param("skip") long skip, @Param("limit") long limit);
-
-	 */
+	Optional<Post> findByPostId(String postId);
 
 	// stream
 	@Query("""
@@ -37,21 +23,10 @@ public interface NeoPostsRepository extends Neo4jRepository<Posts, String> {
 		    OPTIONAL MATCH (p)-[sim:SIMILAR]->(sp:Post)
 		    RETURN p, collect(pr), collect(c), collect(sim), collect(sp)
 		""")
-	Stream<Posts> streamAllWithPromotesAndSimilar();
-
-	/*
-	// 기존 // 데이터 많으면 실행 X
-	@Query("""
-		    MATCH (p:Post)
-		    OPTIONAL MATCH (p)-[pr:PROMOTES]->(c:Channel)
-		    OPTIONAL MATCH (p)-[sim:SIMILAR]->(sp:Post)
-		    RETURN p, collect(pr), collect(c), collect(sim), collect(sp)
-		""")
-	List<Posts> findAllWithPromotesAndSimilar();
-
-	 */
+	Stream<Post> streamAllWithPromotesAndSimilar();
 
 	// postId 설정
 	@Query("MATCH (p:Post) WHERE p.content = $content SET p.postId = $postId RETURN p")
-	Optional<Posts> updatePostIdByContent(@Param("content") String content, @Param("postId") String postId);
+	Optional<Post> updatePostIdByContent(@Param("content") String content, @Param("postId") String postId);
+
 }

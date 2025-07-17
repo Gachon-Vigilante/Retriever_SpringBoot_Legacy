d4b841659a7306be3ffc211fe0ec6f6bb126c022
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-17 22:57:58 +0900

    Delete: 미사용 `util` 삭제

--- src/main/java/com/team7/retriever/util/RegexUtils.java
+++
@@ -1,9 +0,0 @@
-package com.team7.retriever.util;
-
-import java.util.regex.Pattern;
-
-public class RegexUtils {
-    public static String escape(String input) {
-        return Pattern.quote(input);
-    }
-}

535581ba0396389e4b8f9d9e0d47abf2c60cb7b4
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-19 19:58:35 +0900

    Refactor: 북마크 서비스 메소드 호출 시, 일관된 변수명 사용 및 사용자 ID 기반 조회 API 추가

--- src/main/java/com/team7/retriever/domain/bookmark/controller/BookmarksController.java
+++ src/main/java/com/team7/retriever/domain/bookmark/controller/BookmarksController.java
@@ -1,11 +1,11 @@
 package com.team7.retriever.domain.bookmark.controller;
 
-import com.team7.retriever.domain.bookmark.controller.dto.request.BookmarksRequest;
 import com.team7.retriever.domain.bookmark.domain.document.Bookmarks;
 import com.team7.retriever.domain.bookmark.service.BookmarksService;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,34 +16,35 @@ import java.util.Optional;
 @RequestMapping("/bookmarks")
 public class BookmarksController {
 
-    private final BookmarksService BookmarksService;
+    private final BookmarksService bookmarksService;
 
     // 모든 북마크 조회
     @GetMapping("/all") /* 241231 수정 */
     public ResponseEntity<List<Bookmarks>> getAllBookmarks() {
-        List<Bookmarks> bookmarks = BookmarksService.getAllBookmarks();
+        List<Bookmarks> bookmarks = bookmarksService.getAllBookmarks();
+        return ResponseEntity.ok(bookmarks);
+    }
+
+    // 특정 유저 ID로 북마크 조회
+    @GetMapping("/user")
+    public ResponseEntity<List<Bookmarks>> getBookmarksByUserId(@AuthenticationPrincipal String userId) {
+        List<Bookmarks> bookmarks = bookmarksService.getBookmarksByUserId(userId);
         return ResponseEntity.ok(bookmarks);
     }
 
     // 특정 북마크 ID로 북마크 조회
     @GetMapping("/id/{id}") /* 241231 수정 */
     public ResponseEntity<Bookmarks> getBookmarkById(@PathVariable String id) {
-        Optional<Bookmarks> bookmark = BookmarksService.getBookmarksById(id);
+        Optional<Bookmarks> bookmark = bookmarksService.getBookmarksById(id);
         return bookmark.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     // 새로운 북마크 추가
-    @PostMapping("/add")
-    public String addBookmark(@RequestBody BookmarksRequest request) {
-        Bookmarks bookmark = new Bookmarks();
-        bookmark.setChannelId(request.getChannelId());
-        bookmark.setUserId(request.getUserId());
-
-        // 나머지 값들 자동 설정
-        bookmark.setDefaultValues();
+    @PostMapping("{channelId}/add")
+    public String addBookmark(@AuthenticationPrincipal String userId, @PathVariable String channelId) {
 
         // 저장
-        BookmarksService.saveBookmark(bookmark);
+        bookmarksService.saveBookmark(Bookmarks.create(channelId, userId));
 
         // test code
         return "Bookmark created successfully";
@@ -53,7 +54,7 @@ public class BookmarksController {
     // 특정 ID의 북마크 삭제 (북마크 아이디)
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteBookmark(@PathVariable String id) {
-        BookmarksService.deleteBookmarkById(id);
+        bookmarksService.deleteBookmarkById(id);
         return ResponseEntity.noContent().build();
     }
 }
